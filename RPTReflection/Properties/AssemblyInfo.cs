fb89759b3c2cd6a9398f936874026af5c2d90801
Caleb Serafin <calebserafin@outlook.com>
2021-03-09 01:17:09 +0200

    Added steps to read me.

--- RPTReflection/Properties/AssemblyInfo.cs
+++ RPTReflection/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RPTReflection")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Allows SQF to examine it's own RPT output. Allows reactions to hidden engine errors.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Caleb Serafin")]
 [assembly: AssemblyProduct("RPTReflection")]
 [assembly: AssemblyCopyright("MIT License Copyright (c) 2021 Caleb Serafin")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
--- RPTReflection/RVEntryPoint.cs
+++ RPTReflection/RVEntryPoint.cs
@@ -2,50 +2,52 @@
 using System.Runtime.InteropServices;
 using RGiesecke.DllExport;
 
-class RVEntryPoint {
+namespace RPTReflection {
+	public class RVEntryPoint {
 
-    public static ExtensionCallback callback;
-    public delegate int ExtensionCallback([MarshalAs(UnmanagedType.LPStr)] string name, [MarshalAs(UnmanagedType.LPStr)] string function, [MarshalAs(UnmanagedType.LPStr)] string data);
-
-#if WIN64
-    [DllExport("RVExtensionRegisterCallback", CallingConvention = CallingConvention.Winapi)]
-#else
-    [DllExport("_RVExtensionRegisterCallback@4", CallingConvention = CallingConvention.Winapi)]
-#endif
-    public static void RVExtensionRegisterCallback([MarshalAs(UnmanagedType.FunctionPtr)] ExtensionCallback func) {
-        callback = func;
-    }
+		public delegate int ExtensionCallback([MarshalAs(UnmanagedType.LPStr)] string name, [MarshalAs(UnmanagedType.LPStr)] string function, [MarshalAs(UnmanagedType.LPStr)] string data);
+		public static ExtensionCallback callback;
+		private static int numberOfCalls = 0;
 
 #if WIN64
-    [DllExport("RVExtensionVersion", CallingConvention = CallingConvention.Winapi)]
+		[DllExport("RVExtensionRegisterCallback", CallingConvention = CallingConvention.Winapi)]
 #else
-    [DllExport("_RVExtensionVersion@8", CallingConvention = CallingConvention.Winapi)]
+		[DllExport("_RVExtensionRegisterCallback@4", CallingConvention = CallingConvention.Winapi)]
 #endif
-    public static void RvExtensionVersion(StringBuilder output, int outputSize) {
-        output.Append("Test-Extension v1.0");
-    }
+		public static void RVExtensionRegisterCallback([MarshalAs(UnmanagedType.FunctionPtr)] ExtensionCallback func) {
+			callback = func;
+		}
 
 #if WIN64
-    [DllExport("RVExtension", CallingConvention = CallingConvention.Winapi)]
+		[DllExport("RVExtensionVersion", CallingConvention = CallingConvention.Winapi)]
 #else
-    [DllExport("_RVExtension@12", CallingConvention = CallingConvention.Winapi)]
+		[DllExport("_RVExtensionVersion@8", CallingConvention = CallingConvention.Winapi)]
 #endif
-    public static void RvExtension(StringBuilder output, int outputSize,
-        [MarshalAs(UnmanagedType.LPStr)] string function) {
-        output.Append(function);
-    }
+		public static void RvExtensionVersion(StringBuilder output, int outputSize) {
+			output.Append("RPTReflection v0.1");
+		}
 
 #if WIN64
-    [DllExport("RVExtensionArgs", CallingConvention = CallingConvention.Winapi)]
+		[DllExport("RVExtension", CallingConvention = CallingConvention.Winapi)]
 #else
-    [DllExport("_RVExtensionArgs@20", CallingConvention = CallingConvention.Winapi)]
+		[DllExport("_RVExtension@12", CallingConvention = CallingConvention.Winapi)]
 #endif
-    public static int RvExtensionArgs(StringBuilder output, int outputSize,
-        [MarshalAs(UnmanagedType.LPStr)] string function,
-        [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.LPStr, SizeParamIndex = 4)] string[] args, int argCount) {
-        foreach (var arg in args) {
-            output.Append(arg);
-        }
-        return 0;
-    }
+		public static void RvExtension(StringBuilder output, int outputSize,
+			[MarshalAs(UnmanagedType.LPStr)] string function) {
+			output.Append(function);
+			output.Append($"This was call number {(++numberOfCalls).ToString("D")}.");
+		}
+
+#if WIN64
+		[DllExport("RVExtensionArgs", CallingConvention = CallingConvention.Winapi)]
+#else
+		[DllExport("_RVExtensionArgs@20", CallingConvention = CallingConvention.Winapi)]
+#endif
+		public static int RvExtensionArgs(StringBuilder output, int outputSize, [MarshalAs(UnmanagedType.LPStr)] string function, [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.LPStr, SizeParamIndex = 4)] string[] args, int argCount) {
+			foreach (var arg in args) {
+				output.Append(arg);
+			}
+			return 0;
+		}
+	}
 }
