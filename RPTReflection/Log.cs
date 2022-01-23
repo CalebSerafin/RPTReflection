e49e368339afdf8990dc3fa9242bdd8f2feda719
Caleb Serafin <calebserafin@outlook.com>
2022-01-23 20:48:31 +0200

    Added Log and Scanner Starting.

---
+++ RPTReflection/Log.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPTReflection {
+	static class Log {
+#if !DEBUG
+		public static void Verbose(string message) {}
+		public static void Info(string message) {}
+		public static void Warning(string message) {}
+		public static void Error(string message) {}
+#else
+		[Conditional ("DEBUG")]
+		public static void Verbose(string message) {
+
+		}
+#endif
+	}
+}
---
+++ RPTReflection/RPTScanner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPTReflection {
+	class RPTScanner {
+		private bool processRunning;
+		public float UpdateFrequency {
+			get { return UpdateFrequency; }
+			set { UpdateFrequency = value; }
+		}
+		public int MyProperty {
+			get { return MyProperty; }
+			set { MyProperty = value; }
+		}
+
+
+
+
+		public RPTScanner() {
+			processRunning = false;
+			UpdateFrequency = 0;
+
+		}
+
+		void Start() {
+			if (processRunning) {
+				throw new InvalidOperationException("The process had already started!");
+			}
+			processRunning = true;
+		}
+	}
+}
